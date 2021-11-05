9ffa4cb1cc65e2a42b5df2b58cd6907e329c9bcd
patychavezromo <patricia.araceli.chavez.romo@gmail.com>
2021-11-04 21:50:31 -0600

    Solicitud de Numero Entero

--- src/ClasePrincipal.java
+++ src/ClasePrincipal.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class ClasePrincipal
 {
     public static void main(String[] args)
     {
-        System.out.println("¡Hola mundo!");
+        System.out.println("BIENVENIDO");
+        Scanner teclado=new Scanner(System.in);
+        int a;
+        System.out.print("Ingresa un numero : ");
+        a=teclado.nextInt();
+        String valor;
+        valor= Integer.toString(a);
+        System.out.print("El numero ingresado es: "+valor);
     }
 }
